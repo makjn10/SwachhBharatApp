185ef2ff01929d692b2a33291cdc48b82ef1f2da
Mayank Jain <makjn10@gmail.com>
2020-03-08 02:44:58 +0530

    Delete main.js

--- public/scripts/main.js
+++
@@ -1,9 +0,0 @@
-document.onreadystatechange = function() { 
-    if (document.readyState !== "complete") { 
-        document.querySelector("body").style.visibility = "hidden"; 
-        document.querySelector("#loaderit").style.visibility = "visible"; 
-    }else { 
-        document.querySelector("#loaderit").style.display = "none"; 
-        document.querySelector("body").style.visibility = "visible"; 
-    } 
-};
